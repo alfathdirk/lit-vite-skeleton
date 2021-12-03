3f49bc597b0d040a70f4d1228fc71505216ebfeb
alfathdirk <alfathdirk@gmail.com>
2021-12-03 08:18:57 +0700

    fix

--- src/screens/home/home.ts
+++ src/screens/home/home.ts
@@ -1,4 +1,4 @@
-import {LitElement, html, css} from 'lit';
+import {LitElement, html} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 @customElement('x-home')
 export class Home extends LitElement {
@@ -12,39 +12,36 @@ export class Home extends LitElement {
     connectedCallback() {
       super.connectedCallback();
       console.log('connected');
-      setTimeout(() => {
-        this.name = 'Skaleup Lit 2';
-      }, 3000);
     }
 
 
     render() {
       return html`
-            <div class="card shadow mb-4">
-                <div class="card-header py-3">
-                  <h6 class="m-0 font-weight-bold text-primary">User Question List</h6>
-                </div>
-                <div class="card-body">
-                  <div class="table-responsive">
-                    <table class="table table-hover">
-                      <thead>
-                        <tr>
-                          <th>#</th>
-                          <th>First Name</th>
-                          <th>Last Name</th>
-                          </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td>1</td>
-                          <td>Mark</td>
-                          <td>Otto</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+              <h6 class="m-0 font-weight-bold text-primary">User Question List</h6>
             </div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-hover">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>First Name</th>
+                      <th>Last Name</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>Mark</td>
+                      <td>Otto</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+        </div>
       `;
     }
 }
